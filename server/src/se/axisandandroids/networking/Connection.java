faa33e755a8756d9c58f5eaf105287f84d78a9ce
Jakob Vilhelm Grundstrom <jakob.grundstrom@gmail.com>
2011-11-10 17:53:51 +0100

    Scribbled some methods in Connection.java

--- server/src/se/axisandandroids/networking/Connection.java
+++ server/src/se/axisandandroids/networking/Connection.java
@@ -5,11 +5,13 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import se.lth.cs.fakecamera.Axis211A;
+
 
 public class Connection {
 	
-	private Socket sock;
-	private InputStream input;
+	private Socket sock;	
+	private InputStream input;		// input and output should be independent
 	private OutputStream output;
 	
 	
@@ -27,42 +29,69 @@ public class Connection {
 			System.err.println("IO-error");
 			System.exit(1);
 		}
+		connect();
 	}
 	
 		
-	public void sendImage(byte[] data) {
-			
+	public void sendImage(byte[] data) {		
+		try {
+			output.write(data, 0, data.length);
+		} catch (IOException e) {
+			System.err.println("IO-error");
+			System.exit(1);
+		}			
 	}
 	
-	public byte[] recvImage() {		
+	public byte[] recvImage() {
 		
-		return null;
+		// Read to a buffer array instead ???
+		
+		byte[] b = new byte[Axis211A.IMAGE_BUFFER_SIZE];
+		int len;
+		
+		try {
+			len = input.read(b, 0, Axis211A.IMAGE_BUFFER_SIZE);			
+		} catch (IOException e) {
+			System.err.println("IO-error");
+			System.exit(1);
+		}			
+		
+		return null; // note
 	}
 	
 	
-	public void sendDisplayMode() {
-		
+	public void sendDisplayMode(int disp_mode) {
+		sendInt(disp_mode); // Sanity Check ?
 	}
 	
-	public int recvDisplayMode() { 
-		
-		return 0; 
+	public int recvDisplayMode() { 		
+		return recvInt(); // Sanity Check ?
 	}
 	
-	public void sendSyncMode() {
-		
+	public void sendSyncMode(int sync_mode) {
+		sendInt(sync_mode); // Sanity Check ?
 	}
 	
 	public int recvSyncMode() { 
-		return 0; 
+		return recvInt();  // Sanity Check ?
 	}
 	
 	public void connect() {
-		
+		System.out.printf("New Connection: %s", sock.getInetAddress().toString());
 	}
 	
 	public void disconnect() {
+		System.out.printf("Disconnected: %s", sock.getInetAddress().toString());
+		sock = null; // etc...
+	}
+	
+	
+	private void sendInt(int nbr) {
 		
+	}
+	
+	private int recvInt() { 
+		return 0; 
 	}
 	
 	
