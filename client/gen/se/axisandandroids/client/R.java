dc166002e8e4fa6d11a8e4c07bb85a92773199e0
Bengt Ericsson <Zolomon@gmail.com>
2011-11-16 14:36:47 +0100

    first service commit.

--- client/gen/se/axisandandroids/client/R.java
+++ client/gen/se/axisandandroids/client/R.java
@@ -13,11 +13,26 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int btnConnect=0x7f050005;
+        public static final int btnDisconnect=0x7f050000;
+        public static final int etHost=0x7f050003;
+        public static final int etHostAndPort=0x7f050001;
+        public static final int etPort=0x7f050004;
+        public static final int rlHostPortConnect=0x7f050002;
+        public static final int tlConnections=0x7f050006;
+        public static final int trConnection1=0x7f050007;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int connection_item=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int connect=0x7f040005;
+        public static final int disconnect=0x7f040004;
         public static final int hello=0x7f040000;
+        public static final int host=0x7f040002;
+        public static final int port=0x7f040003;
     }
 }
