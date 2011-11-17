0625acb75a5ae8467c3f5e6724d9c11820963605
Carl Christian Bjerggaard <carl.bjerggaard@gmail.com>
2011-11-17 12:13:25 +0100

    Deleted files from cache. Etc.

--- server/src/se/axisandandroids/testconnection/ClientHandler.java
+++ server/src/se/axisandandroids/testconnection/ClientHandler.java
@@ -6,7 +6,6 @@ package se.axisandandroids.testconnection;
  * 
  * --------------------------------------------------- */
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,56 +14,56 @@ import java.net.Socket;
 
 import se.axisandandroids.networking.Connection;
 
-
 public class ClientHandler extends Thread {
 	Socket clientSocket;
-	
-	
+
 	public ClientHandler(Socket clientSocket) {
 		super();
 		this.clientSocket = clientSocket;
 	}
-	
+
 	public void run() {
 		try {
-			//servClient(clientSocket);
-			servConnectionTest(clientSocket);			
+			// servClient(clientSocket);
+			servConnectionTest(clientSocket);
 		} catch (IOException e) {
 			System.err.println("io-exception");
 			System.exit(1);
 		}
 	}
-	
+
 	public void servConnectionTest(Socket sock) throws IOException {
-		Connection con = new Connection(sock);		
-		
+		Connection con = new Connection(sock);
+
 		// Test recvInt()
 		int nbr = con.recvInt();
-		System.out.printf("Got int: %d\n", nbr);	
-		
+		System.out.printf("Got int: %d\n", nbr);
+
 		// Test sendInt()
 		nbr = 123123;
 		System.out.printf("Sending int: %d\n", nbr);
 		con.sendInt(nbr);
-		
-		// Test 
+
+		// Test
 	}
-	
-	
-	public void servClient(Socket clientSocket) throws IOException {	
-		System.out.println("Serving client: " + clientSocket.getInetAddress().toString());
-		
+
+	public void servClient(Socket clientSocket) throws IOException {
+		System.out.println("Serving client: "
+				+ clientSocket.getInetAddress().toString());
+
 		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				clientSocket.getInputStream()));
 
 		String inputLine, outputLine;
 		while ((inputLine = in.readLine()) != null) {
-			System.out.println(inputLine);			
-			outputLine = "[Ether] " + inputLine; 
+			System.out.println(inputLine);
+			outputLine = "[Ether] " + inputLine;
 			out.println(outputLine);
 		}
-		
-		System.out.println("Client Disconnected: " + clientSocket.getInetAddress().toString());
+
+		System.out.println("Client Disconnected: "
+				+ clientSocket.getInetAddress().toString());
 	}
-	
+
 }
