461deea407315e443c1a58f76e27abe14b9ea644
Carl Christian Bjerggaard <carl.bjerggaard@gmail.com>
2011-11-21 14:32:08 +0100

    added subthreads

---
+++ server/src/se/axisandandroids/server/ReceiveThread.java
@@ -0,0 +1,13 @@
+package se.axisandandroids.server;
+
+import se.axisandandroids.networking.Connection;
+import se.axisandandroids.networking.ReceiveThreadSkeleton;
+
+public class ReceiveThread extends ReceiveThreadSkeleton{
+
+	public ReceiveThread(Connection c) {
+		super(c);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ server/src/se/axisandandroids/server/SendThread.java
@@ -0,0 +1,13 @@
+package se.axisandandroids.server;
+
+import se.axisandandroids.networking.Connection;
+import se.axisandandroids.networking.SendThreadSkeleton;
+
+public class SendThread extends SendThreadSkeleton {
+
+	public SendThread(Connection c) {
+		super(c);
+		// TODO Auto-generated constructor stub
+	}
+
+}
