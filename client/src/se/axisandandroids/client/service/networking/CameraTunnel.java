b6d5129e13ad288d2f939a31042cdd2e35b92d0e
Jakob Vilhelm Grundstrom <jakob.grundstrom@gmail.com>
2011-11-23 18:18:35 +0100

    Timestamp decoded

--- client/src/se/axisandandroids/client/service/networking/CameraTunnel.java
+++ client/src/se/axisandandroids/client/service/networking/CameraTunnel.java
@@ -9,26 +9,40 @@ import se.axisandandroids.networking.Connection;
 
 public class CameraTunnel {
 	
-	public Connection connection;	
-
+	public Connection connection;		
 	private DisplayMonitor disp_monitor;
+	private DisplayThread disp_thread;
 	private ClientSendThread send_thread;
 	private ClientReceiveThread recv_thread;
+	
 	
 	public CameraTunnel(Connection c, DisplayMonitor disp_monitor) {
 		this.connection  = c;				
 		this.disp_monitor = disp_monitor;
+		createThreads();
 	}
 	
-	private void createThreads() {
-		// Create the threads here !!!
-		
-		DisplayThread disp_thread = new DisplayThread();		
+	private void createThreads() {		
+		System.out.println("Creating Threads: DisplayThread, ReceiveThread, SendThread...");
+		disp_thread = new DisplayThread(disp_monitor);		
 		recv_thread = new ClientReceiveThread(connection, disp_monitor, disp_thread.mailbox);
 		send_thread = new ClientSendThread(connection);
-	
 	}
 	
+	private void startThreads() {		
+		System.out.println("Starting Threads: DisplayThread, ReceiveThread, SendThread...");
+		disp_thread.start();
+		recv_thread.start();
+		send_thread.start();
+	}
+	
+	private void interruptThreads() {		
+		System.out.println("Interrupting Threads: DisplayThread, ReceiveThread, SendThread...");
+		disp_thread.interrupt();
+		recv_thread.interrupt();
+		send_thread.interrupt();
+	}
+		
 	public CircularBuffer getSendMailbox() {
 		return send_thread.mailbox;
 	}
