fcb7d8b009c533b04aef1d3d384d8f68c5bed078
Jakob Vilhelm Grundstrom <jakob.grundstrom@gmail.com>
2011-11-24 02:18:11 +0100

    display monitor hacking time

--- client/src/se/axisandandroids/client/display/DisplayMonitor.java
+++ client/src/se/axisandandroids/client/display/DisplayMonitor.java
@@ -15,25 +15,61 @@ public class DisplayMonitor {
 
 	private int disp_mode = Protocol.DISP_MODE.AUTO;
 	private int sync_mode = Protocol.SYNC_MODE.AUTO;
-	private long showtime_old = 0;
-	private long timestamp_old;
+	
 
 	public DisplayMonitor() {
 		camera = new Axis211A();
 	}
 
-	public synchronized long syncFrames(long timestamp) throws InterruptedException {
-		if (showtime_old <= 0) return 0;
-
-		long showtime_new = showtime_old + (timestamp - timestamp_old);
-		long diffTime = showtime_new - System.currentTimeMillis();
-		while ((diffTime = showtime_new - System.currentTimeMillis()) > 0) {
+	
+	/* Generalize this for other than 2 DisplayThreads !!! */
+	private final long[] timestamps = new long[2];
+	private final long[] showtime_old = new long[2];
+	private final long[] timestamp_old = new long[2];
+	
+	
+	public synchronized long syncFrames(int id, long timestamp) throws InterruptedException {
+		
+		/* No old showtime exists, display now! */
+		if (showtime_old[id] <= 0) return System.currentTimeMillis() - timestamp;
+		
+		long showtime_new = showtime_old[id] + (timestamp - timestamp_old[id]);
+		long diffTime;
+		
+		if (sync_mode == Protocol.SYNC_MODE.ASYNC) {
+						
+			/* Wait until it is:
+			 * 1) The right time.										*/
+			while ((diffTime = showtime_new - System.currentTimeMillis()) > 0) {
 				wait(diffTime);		
+			}
+						
+		} else if (sync_mode == Protocol.SYNC_MODE.SYNC) {
+			
+			/* Generalize this for other than 2 DisplayThreads !!! */
+			timestamps[id] = timestamp; // Register timestamp for comparision	
+			long first_timestamp = timestamps[0] < timestamps[1] ? timestamps[0] : timestamps[1];
+
+			/* Wait until it is:
+			 * 1) The right time.
+			 * 2) timestamp less than all other timestamps.				*/
+			while ((diffTime = showtime_new - System.currentTimeMillis()) > 0 
+					&& timestamp <= first_timestamp) {
+				wait(diffTime);		
+			}
+			
+			timestamps[id] = Long.MAX_VALUE; // This timestamp is done
+
 		}
+		
+		timestamp_old[id] = timestamp;
+		showtime_old[id] = showtime_new;
 
-
-		showtime_old = showtime_new;
-		return System.currentTimeMillis() - timestamp; // The real delay
+		/* Calculate delay and determine sync mode */
+		long delay = System.currentTimeMillis() - timestamp; 	// The real delay
+		if (delay > 200) sync_mode = Protocol.SYNC_MODE.ASYNC;	// delay determines sync mode
+		else sync_mode = Protocol.SYNC_MODE.SYNC;
+		return delay; 											// Return delay to show
 	}
 
 	public synchronized void connect() {
