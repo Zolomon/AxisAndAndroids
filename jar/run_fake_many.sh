99dc3a01f1d9b29babf38de9109ae9aa9b6ddecd
Jakob Vilhelm Grundstrom <jakob.grundstrom@gmail.com>
2011-11-30 19:40:27 +0100

    more runscript fixes

--- jar/run_fake_many.sh
+++ jar/run_fake_many.sh
@@ -3,4 +3,4 @@ java -jar FakeCameraServer.jar 6000 &
 java -jar FakeCameraServer.jar 6001 &
 java -jar FakeCameraServer.jar 6002 &
 
-java -jar DesktopClient.jar localhost 6000 localhost 6000 localhost 6001 localhost 6001 localhost 6002 localhost 6002 &
+java -jar DesktopClient_standard.jar localhost 6000 localhost 6000 localhost 6001 localhost 6001 localhost 6002 localhost 6002 &
